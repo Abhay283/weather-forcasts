516b754e10fc09a4457db9fe654740ecabd75835
Abhay Gupta <abhaygupta4235@gmail.com>
2025-01-27 17:01:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './test.css';
 import Test from './test';
-// import Demo from './demo';
-// import Clim from './clim';
-// import Dict from './dict';
 function App(){
   return(
     <div>
       <Test/>
-      {/* <Demo/> */}
-      {/* <Clim/> */}
-      {/* <Dict/> */}
     </div>
   )
 };
